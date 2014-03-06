51989e7b43c61c631b74c189e2969b6547a595b6
travisb22 <travis.britt@ymail.com>
2014-03-06 14:48:41 -0500

    Lab2
    3/6/14

---
+++ Britt_Travis_lab2.js
@@ -0,0 +1,27 @@
+//alert("JavaScript works!");
+
+//variables
+var myVideoGame = "League of Legends";
+var myComputerName = "\"Travis's\" Mac-Book Pro";
+var myLeagueGamesPlayed = 297;
+var doIPlayToday = true;
+
+//My outputs
+console.log("I play the game " + myVideoGame + "." );
+console.log("I play the game on my computer, " + myComputerName + ".");
+console.log("I have played " + myLeagueGamesPlayed + " games in League of Legends.");
+console.log("It is " + doIPlayToday + ", I will play today.");
+
+doIPlayToday = confirm("Will I play League of Legends today?");
+
+console.log("It is " + doIPlayToday + " that I will play for 3 hours today.");
+
+myLeagueGamesPlayed = prompt("Enter new number of games played: ","300");
+
+myLeagueGamesPlayed = parseInt(myLeagueGamesPlayed);
+
+console.log("My new number of games plus 10 is " + (myLeagueGamesPlayed + 10) + ".");
+
+ 
+
+ 
